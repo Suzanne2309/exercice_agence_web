c97503700c2766eb60f64528a17b0cf94598d780
Suzanne Pons <suzanne.pons68@gmail.com>
2025-09-16 14:37:47 +0200

    Ajustement du burger, et notamment de l'affichage du menu (mobile)

--- js/main.js
+++ js/main.js
@@ -1,44 +1,8 @@
 // console.log("hello world") inscrit après la création du fichier, pour s'assurer que le fichier soit bien prit en compte par le navigateur
 
-//Test création d'événement avec nouvelle div
-
-//Test active /*
-// const burger = document.querySelector('#burger');
-// const menu = document.querySelector('.principale')
-// const links = document.querySelector('#links');
-
-// const active = document.createElement("div")
-// active.classList.add("active")
-
-// burger.addEventListener('click', () => {
-//     menu.classList.toggle('active');
-// })
-
-
-
-// Solution qui fonctionne
-// On définit la variable menu = nav principale en passant par le DOM
-//     const menu = document.getElementById('principale')
-// // Création de l'événement click => class = active
-//     burger.addEventListener('click' , () => {
-//         if (menu.classList.contains("active")) {
-//             menu.style.display = "block";
-     
-//     } else {
-//           menu.classList.toggle("active")
-//         // menu.style.display = "none";
-//     }
-   
-// })
-// Fonction si class active alors affichage menu mobile et si pas active alors rien afficher
-// function navBurger() {
-    
-// }
-
 
 // Création d'événement click
 const burger = document.getElementById('burger');
-
 const menu = document.getElementById('principale');
  
 burger.addEventListener('click', () => {
@@ -54,7 +18,7 @@ burger.addEventListener('click', () => {
 })
 
 
-    //Switch de bars vers croix
+    //Switch de bars vers croix et inversement (ouvrir et fermer le menu)
 const bars = document.querySelector('.bars');
 const croix = document.querySelector('.croix');
 
